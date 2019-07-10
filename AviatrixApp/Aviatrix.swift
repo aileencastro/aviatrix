4245f58f917c5b08634b1486a964fcc3def8de55
aileencastro <aileencastrogg@gmail.com>
2019-07-10 11:55:28 -0700

    Complete Iteration 0

--- AviatrixApp/Aviatrix.swift
+++ AviatrixApp/Aviatrix.swift
@@ -10,8 +10,22 @@ import Foundation
 
 class Aviatrix {
     
-    func start() -> Bool {
-        return true
+    
+    
+    //saying who created this plane and airline
+    var author = ""
+    //reads in the author
+    init(myAuthor:String){
+        author = myAuthor
+    }
+    
+    //this function should tell us if the airplane is runnin or not
+    var running = false
+    
+    //starts the airplane so it means it's running
+    func start() -> Bool { //bool stands for boolean which means true or false
+        running = true
+        return running
     }
     
     func refuel() {
